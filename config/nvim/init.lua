da46983bf5999e16f649d5356872f6d08a1b13f1
Ana Hobden <operator@hoverbear.org>
2021-12-26 13:39:01 -0800

    Add lspconfig
    Signed-off-by: Ana Hobden <operator@hoverbear.org>

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -42,7 +42,7 @@ bo.swapfile = false
 bo.undofile = true
 -- Keep undo history across sessions by storing it in a file
 if not (vim.fn.empty(vim.fn.glob(o.undodir)) > 0) then
-  os.execute('mkdir -p' .. o.undodir .. '-m=0770')
+    os.execute('mkdir -p' .. o.undodir .. '-m=0770')
 end
 
 -- Vim commands
@@ -59,3 +59,30 @@ keymap('t', '<Esc>', [[<C-\><C-n>]], {})
 keymap('n', [[<C-g>]], [[<cmd>lua require('telescope.builtin').find_files()<cr>]], {})
 keymap('n', [[<C-f>]], [[<cmd>lua require('telescope.builtin').live_grep()<cr>]], {})
 
+-- LSP settings
+local lspconfig = require('lspconfig')
+
+local on_attach = function(client, bufnr)
+    local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+    buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+    local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+    local opts = { noremap=true, silent=true }
+    buf_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+    buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+    buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+    buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+    buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+    buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+    buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+    buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+    buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+    buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+    buf_set_keymap('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+    buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+    buf_set_keymap('n', '<space>e', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
+    buf_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
+    buf_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
+    buf_set_keymap('n', '<space>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
+    buf_set_keymap('n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+end
+lspconfig.rust_analyzer.setup { on_attach = on_attach }
--- packages/neovim.nix
+++ packages/neovim.nix
@@ -16,6 +16,7 @@ neovim.override {
         telescope-nvim
         plenary-nvim
         popup-nvim
+        nvim-lspconfig
       ];
     };
   };
