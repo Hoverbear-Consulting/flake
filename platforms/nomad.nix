357cce2b9b13edff850ddf4d6f134246188abc92
Ana Hobden <operator@hoverbear.org>
2022-08-30 12:09:30 -0700

    Add Shadow link
    Signed-off-by: Ana Hobden <operator@hoverbear.org>

--- platforms/nomad.nix
+++ platforms/nomad.nix
@@ -144,13 +144,14 @@ in
     adjtime.source = "/persist/etc/adjtime";
     # NIXOS.source = "/persist/etc/NIXOS";
     machine-id.source = "/persist/etc/machine-id";
+    shadow.source = "/persist/etc/shadow";
   };
   systemd.tmpfiles.rules = [
     "L /var/lib/NetworkManager/secret_key - - - - /persist/var/lib/NetworkManager/secret_key"
     "L /var/lib/NetworkManager/seen-bssids - - - - /persist/var/lib/NetworkManager/seen-bssids"
     "L /var/lib/NetworkManager/timestamps - - - - /persist/var/lib/NetworkManager/timestamps"
     "L /etc/secrets - - - - /persist/secrets"
-    "L /var/lib/bluetooth - - - - /persist/var/lib/bluetooth"
+    #"L /var/lib/bluetooth - - - - /persist/var/lib/bluetooth"
   ];
   security.sudo.extraConfig = ''
     # rollback results in sudo lectures after each reboot
