f98b51c0a0bd774ae1b89f229963e281558ec242
Ana Hobden <operator@hoverbear.org>
2022-09-10 13:32:12 -0700

    Update architect
    Signed-off-by: Ana Hobden <operator@hoverbear.org>

--- platforms/architect.nix
+++ platforms/architect.nix
@@ -1,16 +1,10 @@
 { config, pkgs, lib, modulesPath, ... }:
 
 let
-  devices = {
-    encrypted = rec {
-      uuid = "102d36f0-1c99-43d3-855b-e448a57ca4e3";
-      label = "architect";
-    };
-    boot = rec {
-      uuid = "9B3C-1A78";
-      label = "boot";
-    };
-  };
+  encryptedDeviceLabel = "encrypt";
+  encryptedDevice = "/dev/nvme1n1p2";
+  efiDevice = "/dev/nvme1n1p1";
+  makeMounts = import ./../functions/make_mounts.nix;
 in
 {
   imports = [
@@ -18,30 +12,11 @@ in
   ];
 
   config = {
-    boot.initrd.luks.devices = {
-      architect = {
-        device = "/dev/disk/by-uuid/${devices.encrypted.uuid}";
-      };
-    };
-
     boot.initrd.kernelModules = [ "amdgpu" ];
     services.xserver.videoDrivers = [ "amdgpu" ];
 
-    fileSystems."/" = {
-      device = "/dev/mapper/${devices.encrypted.label}";
-      fsType = "f2fs";
-      encrypted.enable = true;
-      encrypted.label = devices.encrypted.label;
-      encrypted.blkDev = "/dev/disk/by-uuid/${devices.encrypted.uuid}";
-      options = [
-        "compress_algorithm=zstd"
-        "atgc"
-        "lazytime"
-      ];
-    };
-    fileSystems."/boot" = {
-      device = "/dev/disk/by-uuid/${devices.boot.uuid}";
-      fsType = "vfat";
+    fileSystems = makeMounts {
+      inherit encryptedDevice encryptedDeviceLabel efiDevice;
     };
 
     networking.hostName = "architect";
