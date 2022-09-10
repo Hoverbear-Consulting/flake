db7e91f48f19dfc9635e8489815bc5cf126ddeeb
Ana Hobden <operator@hoverbear.org>
2022-09-09 22:22:19 -0700

    Touchup
    Signed-off-by: Ana Hobden <operator@hoverbear.org>

--- platforms/nomad.nix
+++ platforms/nomad.nix
@@ -15,17 +15,6 @@ in
       "dev.i915.perf_stream_paranoid" = 0;
     };
     boot.loader.efi.efiSysMountPoint = "/boot/efi";
-    boot.initrd.luks.devices = {
-      encrypt = {
-        device = "/dev/disk/by-uuid/${encryptedDeviceLabel}";
-        keyFile = "/keyfile.bin";
-        allowDiscards = true;
-      };
-    };
-    boot.initrd.secrets = {
-      "keyfile.bin" = "/etc/secrets/initrd/keyfile.bin";
-    };
-
     fileSystems = makeMounts {
       inherit encryptedDeviceLabel efiDeviceLabel;
     };
