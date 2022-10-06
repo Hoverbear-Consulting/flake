1167097b65d9f58858b463f041b93aa383fcdf3b
Ana Hobden <operator@hoverbear.org>
2022-10-06 07:52:26 -0700

    Repair shellcheck stuff
    Signed-off-by: Ana Hobden <operator@hoverbear.org>

--- packages/fix-vscode/fix-vscode.sh
+++ packages/fix-vscode/fix-vscode.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env nix-shell 
 #!nix-shell -i bash -p patchelf
 
-find ~/.vscode-server  -print0 -name node -type f \
-	| xargs patchelf \
+find ~/.vscode-server -print0 -name node -type f \
+	| xargs --null patchelf \
 		--set-interpreter "$(patchelf --print-interpreter "$(which patchelf)")" \
 		--set-rpath "$(dirname "$(ldd "$(which patchelf)" | grep stdc++ | cut -d' ' -f3)")"
 
-find ~/.vscode-server  -print0 -name rg -type f \
-	| xargs patchelf \
+find ~/.vscode-server -print0 -name rg -type f \
+	| xargs --null patchelf \
 		--set-interpreter "$(patchelf --print-interpreter "$(which patchelf)")" \
 		--set-rpath "$(dirname "$(ldd "$(which patchelf)" | grep stdc++ | cut -d' ' -f3)")"
